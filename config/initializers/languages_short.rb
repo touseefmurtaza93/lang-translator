38a4f045e405b2978afeedadd261e301fccbe868
Touseef Murtaza <touseefmurtaza1993@gmail.com>
2020-12-30 00:30:03 +0500

    record which languages are not supported by rapid api

--- config/initializers/languages_short.rb
+++ config/initializers/languages_short.rb
@@ -1,1 +1,8 @@
 LANGUAGES ||= ['ar', 'bn', 'ca', 'cs', 'da', 'de', 'es', 'est', 'fa', 'fi', 'fr', 'hi', 'hr', 'hu', 'it', 'jp', 'ko', 'ma', 'ml', 'nl', 'no', 'pa', 'pl', 'pt', 'ru', 'sv', 'te', 'tr', 'uk', 'ur', 'vi', 'zh']
+
+LANGUAGES_WHICH_ARE_NOT_GETTING_TRANSLATED = {
+  jp: :japanese,
+  ma: :malay,
+  ml: :malay,
+  te: :telugu
+}
